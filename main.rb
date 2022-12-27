a771beff7f757897f67b23a628283b42f03284c2
vutuankiet4599 <vutuankiet4599@gmail.com>
2022-12-27 11:10:25 +0700

    Rubyの課題１と２

--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
 puts 'K64の皆さん、こんにちは!'
 puts 'Hello world with Ruby in Repl IT'
+
+users = {
+    0 => {:username => "admin", :password => "123456"},
+    1 => {:username => "user", :password => "123456"}
+}
+
+users.map do |key, user|
+    puts "#{user[:username]} - #{user[:password]}"
+end
